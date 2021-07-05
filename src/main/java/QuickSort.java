40ca5c50f51f5b083f50629771b53fdde2e58dd8
Luca Cirillo <luca.cirillo@sisal.it>
2021-07-05 14:51:06 +0200

    daje

---
+++ src/main/java/QuickSort.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuickSort {
+    List<Integer> numbers;
+
+
+    public List<Integer> execute(){
+        return step(numbers);
+    }
+
+
+    public List<Integer> step(List<Integer> numbers){
+
+        if(numbers.size() <= 1){
+            return numbers;
+        }
+
+        int pivot = numbers.get(0);
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        for(Integer n : numbers){
+            if(n != pivot){
+                if(n < pivot){
+                    left.add(n);
+                }else{
+                    right.add(n);
+                }
+            }
+        }
+
+        List<Integer> res = step(left);
+        res.add(pivot);
+        res.addAll(step(right));
+
+        return res;
+    }
+
+    public void init(List<Integer> numbers){
+        this.numbers = numbers;
+    }
+}
