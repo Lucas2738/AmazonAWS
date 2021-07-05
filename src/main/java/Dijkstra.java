aad38c5ad0e3b3e3b132975f231ce90ae989cdc7
Luca Cirillo <luca.cirillo@sisal.it>
2021-07-05 09:53:57 +0200

    D

--- src/main/java/Dijkstra.java
+++ src/main/java/Dijkstra.java
@@ -1,99 +1,106 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 import static java.lang.System.*;
 
 public class Dijkstra {
 
-    private final List<Edge> edges =  new ArrayList<>();
-
-    private static Node start;
-
-    private final HashMap<Integer, Node> shortestPath = new HashMap<>();
-    private final HashMap<Integer, Node> visited = new HashMap<>();
+    private final HashMap<String, Node> visited = new HashMap<>();
+    private Set<Node> unVisited = new HashSet<>();
 
     public static void main(String[] args){
         Dijkstra dijkstra =  new Dijkstra();
         dijkstra.init();
-        dijkstra.run(start);
+        dijkstra.run();
 
         out.println("END");
     }
 
 
-    private void run(final Node node){
-        shortestPath.putIfAbsent(node.getKey(), node);
+    private void run(){
 
-        List<Edge> neighs = edges.stream().filter(e -> e.getNodea().getKey() == node.getKey() || e.getNodeb().getKey() == node.getKey()).collect(Collectors.toList());
-        neighs.forEach(n -> {
-                    Node b = getOtherEdgeNode(n, node);
+        while(unVisited.size() != 0){
+            Node node = getLowestDistanceNode();
+            unVisited.remove(node);
 
-                    if(shortestPath.get(b.getKey()) != null){
-                        b = shortestPath.get(b.getKey());
-                    }
-
-                    Node no = node;
-                    if(shortestPath.get(node.getKey()) != null){
-                        no = shortestPath.get(node.getKey());
-                    }
-
-                    if((no.getDistance() + n.getWeight() < b.getDistance())){
-                        b.setPrevious(no);
-                        b.setDistance(no.getDistance() + n.getWeight());
-                    }
-
-                    shortestPath.putIfAbsent(b.getKey(), b);
+            for (Map.Entry<Node, Integer> adjacencyPair : node.getAdjacentNodes().entrySet()) {
+                if((node.getDistance() + adjacencyPair.getValue() < adjacencyPair.getKey().getDistance())){
+                    LinkedList<Node> shortestPath = new LinkedList<>(node.getShortestPath());
+                    shortestPath.add(node);
+                    adjacencyPair.getKey().setShortestPath(shortestPath);
+                    adjacencyPair.getKey().setDistance(node.getDistance() + adjacencyPair.getValue());
+                }
             }
-        );
-
-        visited.put(node.getKey(), node);
-
-        neighs.stream()
-                .filter(n -> visited.get(getOtherEdgeNode(n, node).getKey()) == null)
-                .map(n -> getOtherEdgeNode(n, node))
-                .forEach(this::run);
+            visited.put(node.getName(), node);
+        }
     }
 
 
-    private Node getOtherEdgeNode(Edge edge, Node node){
-        Node result;
-        if(edge.getNodea().equals(node)){
-            result = edge.getNodeb();
-        }else{
-            result = edge.getNodea();
+
+    private Node getLowestDistanceNode(){
+        Node result = null;
+        Integer dist = Integer.MAX_VALUE;
+
+        for(Node n : unVisited){
+            if(n.getDistance() < dist){
+                dist = n.getDistance();
+                result = n;
+            }
         }
         return result;
     }
 
     private void init(){
-        Node a = Node.builder().key(1).distance(0).build();
-        Node b = Node.builder().key(2).distance(999).build();
-        Node c = Node.builder().key(3).distance(999).build();
-        Node d = Node.builder().key(4).distance(999).build();
-        Node e = Node.builder().key(5).distance(999).build();
-        Node f = Node.builder().key(6).distance(999).build();
-        Node g = Node.builder().key(7).distance(999).build();
-        Node h = Node.builder().key(8).distance(999).build();
-        Node i = Node.builder().key(9).distance(999).build();
+        Node a = new Node("A");
+        a.setDistance(0);
+        Node b = new Node("B");
+        Node c = new Node("C");
+        Node d = new Node("D");
+        Node e = new Node("E");
+        Node f = new Node("F");
+        Node g = new Node("G");
+        Node h = new Node("H");
+        Node i = new Node("I");
 
-        start = a;
 
-        edges.add(Edge.builder().weight(10).nodea(a).nodeb(b).build());
-        edges.add(Edge.builder().weight(12).nodea(a).nodeb(c).build());
-        edges.add(Edge.builder().weight(8).nodea(b).nodeb(e).build());
-        edges.add(Edge.builder().weight(9).nodea(b).nodeb(c).build());
-        edges.add(Edge.builder().weight(3).nodea(c).nodeb(d).build());
-        edges.add(Edge.builder().weight(1).nodea(c).nodeb(f).build());
-        edges.add(Edge.builder().weight(7).nodea(d).nodeb(e).build());
-        edges.add(Edge.builder().weight(3).nodea(d).nodeb(f).build());
-        edges.add(Edge.builder().weight(8).nodea(e).nodeb(g).build());
-        edges.add(Edge.builder().weight(5).nodea(e).nodeb(h).build());
-        edges.add(Edge.builder().weight(6).nodea(f).nodeb(h).build());
-        edges.add(Edge.builder().weight(2).nodea(g).nodeb(i).build());
-        edges.add(Edge.builder().weight(9).nodea(g).nodeb(h).build());
-        edges.add(Edge.builder().weight(11).nodea(h).nodeb(i).build());
+        a.getAdjacentNodes().put(b, 10);
+        a.getAdjacentNodes().put(c, 12);
+        b.getAdjacentNodes().put(a, 10);
+        c.getAdjacentNodes().put(a, 12);
+        b.getAdjacentNodes().put(e, 8);
+        b.getAdjacentNodes().put(c, 9);
+        e.getAdjacentNodes().put(b, 8);
+        c.getAdjacentNodes().put(b, 9);
+        c.getAdjacentNodes().put(d, 3);
+        c.getAdjacentNodes().put(f, 1);
+        d.getAdjacentNodes().put(c, 3);
+        f.getAdjacentNodes().put(c, 1);
+        d.getAdjacentNodes().put(e, 7);
+        d.getAdjacentNodes().put(f, 3);
+        e.getAdjacentNodes().put(d, 7);
+        f.getAdjacentNodes().put(d, 3);
+        e.getAdjacentNodes().put(g, 8);
+        e.getAdjacentNodes().put(h, 5);
+        g.getAdjacentNodes().put(e, 8);
+        h.getAdjacentNodes().put(e, 5);
+        f.getAdjacentNodes().put(h, 6);
+        h.getAdjacentNodes().put(f, 6);
+        g.getAdjacentNodes().put(i, 2);
+        g.getAdjacentNodes().put(h, 9);
+        h.getAdjacentNodes().put(g, 9);
+        i.getAdjacentNodes().put(g, 2);
+        h.getAdjacentNodes().put(i, 11);
+        i.getAdjacentNodes().put(h, 11);
+
+
+        unVisited.add(a);
+        unVisited.add(b);
+        unVisited.add(c);
+        unVisited.add(d);
+        unVisited.add(e);
+        unVisited.add(f);
+        unVisited.add(g);
+        unVisited.add(h);
+        unVisited.add(i);
 
     }
 
--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,14 +1,31 @@
-import lombok.Builder;
 import lombok.Data;
 
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 @Data
-@Builder
 public class Node {
-    int key;
-    Node previous;
-    int distance;
+    private String name;
+    private LinkedList<Node> shortestPath =  new LinkedList<>();
+    private int distance = Integer.MAX_VALUE;
+    private Map<Node, Integer> adjacentNodes =  new HashMap<>();
+
+
+    public Node(String name){
+        this.name = name;
+    }
 
     public boolean equals(Node n){
-        return n.getKey() == this.key;
+        return n.getName() == this.name;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder path = new StringBuilder();
+        shortestPath.forEach(s -> path.append(s.getName() + " -> "));
+        path.append(name);
+        return MessageFormat.format("Node: {0}, shortestPath: {1}, distance: {2}", name, path , distance);
     }
 }
