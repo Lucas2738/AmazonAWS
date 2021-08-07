f5abd52c1718b0189feed0f074254a596659b7c1
Luca Cirillo <luca.cirillo@sisal.it>
2021-08-07 13:19:38 +0200

    binaryt search

---
+++ src/main/java/alogorithm/BinarySearch.java
@@ -0,0 +1,24 @@
+package alogorithm;
+
+import java.util.List;
+
+public class BinarySearch {
+
+    public static Integer execute(Integer num, List<Integer> nums){
+        int inf = 0;
+        int sup = nums.size();
+        int i;
+        while(true){
+            i = (sup+inf)/2;
+            int current = nums.get(i);
+            if(current < num){
+                inf = i;
+            }else if (current > num){
+                sup = i;
+            }else {
+                return i;
+            }
+        }
+    }
+
+}
---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,18 @@
+import alogorithm.BinarySearch;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SearchTest {
+
+    @Test
+    public void binarySearch(){
+        List<Integer> nums = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26));
+        int index = BinarySearch.execute(24, nums);
+
+        Assertions.assertTrue(index == 23);
+    }
+}
