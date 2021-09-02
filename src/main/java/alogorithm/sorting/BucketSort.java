f897c8573143d1ffc928d286ba849ae11691dd33
Luca Cirillo <luca.cirillo@sisal.it>
2021-09-02 15:07:56 +0200

    doc

--- src/main/java/alogorithm/sorting/BucketSort.java
+++ src/main/java/alogorithm/sorting/BucketSort.java
@@ -5,7 +5,7 @@ import static java.util.Map.*;
 import static java.util.AbstractMap.*;
 
 public class BucketSort {
-    private static final int BUCKET_NUM = 10;
+    private static final int BUCKET_NUM = 60;
     List<Integer> numbers;
     List<Integer> result = new ArrayList<>();
 
@@ -15,32 +15,36 @@ public class BucketSort {
 
     private List<Integer> step(List<Integer> nums){
         List<Entry<Integer, List<Integer>>> buckets;
-
+        int max = Integer.MIN_VALUE;
+        for (Integer n : nums) {
+            if (n > max) {
+                max = n;
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for (Integer n : nums) {
+            if (n < min) {
+                min = n;
+            }
+        }
         int bucketNums = BUCKET_NUM;
         if(nums.size() >= bucketNums) {
-            buckets = buildBucket(nums, BUCKET_NUM);
+            buckets = buildBucket(nums, BUCKET_NUM, min, max);
         }else if(nums.size() == 1){
             result.add(nums.get(0));
             return result;
         }else{
-            buckets = buildBucket(nums, nums.size()/2);
+            bucketNums = nums.size()/2;
+            buckets = buildBucket(nums, nums.size()/2, min, max);
         }
 
         for (Integer num : nums) {
-            Integer key = -1;
-            int k = -1;
-            int i = buckets.size()-1;
-            while(i>=0){
-                Map.Entry<Integer, List<Integer>> b = buckets.get(i);
-                if(num <= b.getKey()){
-                    key = b.getKey();
-                    k = i;
-                }
-                i--;
+            int i = num / (max / bucketNums);
+            if(i >= buckets.size()){
+                i = buckets.size() - 1;
             }
-            if(key != -1){
-                buckets.get(k).getValue().add(num);
-            }
+            buckets.get(i).getValue().add(num);
+
         }
 
         for(int i = 0; i < buckets.size(); i++){
@@ -51,22 +55,10 @@ public class BucketSort {
         return result;
     }
 
-    private List<Entry<Integer, List<Integer>>> buildBucket(List<Integer> nums, int bucketNums){
+    private List<Entry<Integer, List<Integer>>> buildBucket(List<Integer> nums, int bucketNums, int min, int max){
         List<Entry<Integer, List<Integer>>> buckets = new ArrayList<>();
 
         if(nums != null && nums.size() > 0) {
-            int max = Integer.MIN_VALUE;
-            for (Integer num : nums) {
-                if (num > max) {
-                    max = num;
-                }
-            }
-            int min = Integer.MAX_VALUE;
-            for (Integer num : nums) {
-                if (num < min) {
-                    min = num;
-                }
-            }
             for (int i = 0; i < bucketNums; i++) {
                 buckets.add(new SimpleEntry<>(min + (max - min) / bucketNums * i, new ArrayList<>()));
             }
